3c55f484f80b480b633acc3c4fe22221ff0b0e95
speedcell4 <speedcell4@gmail.com>
2018-08-19 13:13:10 +0900

    refactor(attention): delay attention matrix normalization

--- torch_attention/facets.py
+++ torch_attention/facets.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-from torch_attention import Attention, masked_fill
+from torch_attention import Attention
 
 
 class Facets(Attention):
@@ -12,12 +12,9 @@ class Facets(Attention):
         self.fc = nn.Linear(in_features1 * 4, 1, bias=bias)
         self.softmax = nn.Softmax(dim=-1)
 
-    def attend(self, Q: torch.Tensor, K: torch.Tensor, mask: torch.ByteTensor = None) -> torch.Tensor:
+    def attend(self, Q: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
         *batches, channel1, in_features1 = Q.size()
         *batches, channel2, in_features1 = K.size()
         Q = Q.view(*batches, channel1, 1, in_features1).expand(*batches, channel1, channel2, in_features1)
         K = K.view(*batches, 1, channel2, in_features1).expand(*batches, channel1, channel2, in_features1)
-        A = self.fc(torch.cat([Q, K, (Q - K).abs(), Q * K], dim=-1)).squeeze(-1)
-        if mask is not None:
-            A = masked_fill(A, mask=mask, filling_value=-float('inf'))
-        return self.softmax(A)
+        return self.fc(torch.cat([Q, K, (Q - K).abs(), Q * K], dim=-1)).squeeze(-1)
