7c0c11b48b4b336782a085a49351058d7559745a
speedcell4 <speedcell4@gmail.com>
2018-08-21 16:56:17 +0900

    test(bilinear): init unit test for BiLinearAttention

--- tests/test_attentions.py
+++ tests/test_attentions.py
@@ -1,7 +1,7 @@
 from hypothesis import given
 
 from tests import *
-from torch_attention import DotProduct, Facets, MultiHead
+from torch_attention import BiLinearAttention, DotProduct, Facets, MultiHead
 
 
 @given(
@@ -87,3 +87,30 @@ def test_multi_head(batches, channel1, channel2, in_features1, in_features2, num
     assert attention.attend(Q, K).size() == A.size()
     assert attention.interact(A, V).size() == (*batches, channel1, out_features)
     assert attention(Q, K, V).size() == (*batches, channel1, out_features)
+
+
+@given(
+    batches=BATCHES,
+    channel1=CHANNEL,
+    channel2=CHANNEL,
+    in_features1=NORMAL_FEATURES,
+    in_features2=NORMAL_FEATURES,
+    in_features3=NORMAL_FEATURES,
+    hidden_size=st.one_of(st.one_of(), NORMAL_FEATURES),
+)
+def test_bilinear(batches, channel1, channel2, in_features1, in_features2, in_features3, hidden_size):
+    attention = BiLinearAttention(in_features1, in_features2, hidden_size)
+    Q = torch.rand(*batches, channel1, in_features1)
+    K = torch.rand(*batches, channel2, in_features2)
+    V = torch.rand(*batches, channel2, in_features3)
+    A = torch.rand(*batches, channel1, channel2)
+
+    attention = attention.to(device)
+    Q = Q.to(device)
+    K = K.to(device)
+    V = V.to(device)
+    A = A.to(device)
+
+    assert attention.attend(Q, K).size() == A.size()
+    assert attention.interact(A, V).size() == (*batches, channel1, in_features3)
+    assert attention(Q, K, V).size() == (*batches, channel1, in_features3)
